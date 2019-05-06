b0cad7a9010e180459555bb2444c6c6ede9390fe
WangZhen <wangzhenjjcn@gmail.com>
2019-05-06 16:28:52 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  version: "1.0.4",
-  note: '小公子财税',
+  version: "2.1.1",
+  note: '小公子集团',
   subDomain: "xgzjt",  
   appid: "wxdfefe35cc7453431", 
-  shareProfile: '从注册就开始的创业伙伴'  
+  shareProfile: '从注册就开始的创业伙伴',
+  
 }
  
